b4f04145b872534a18493821fca0d80e27b0cef8
Shivang Suchak <shivang170695@gmail.com>
2021-01-10 23:09:04 -0500

    Understanding Regular expression in JavaScript

---
+++ js-basics/regularExpression.js
@@ -0,0 +1,14 @@
+/* 
+    Introduction: Regular expression are special strings that represent a search pattern. Also known as regex, it helps programmer match, search, or replace text.
+
+    Regular expression can be cryptic because a few characters have special meaning. The goal is to combine the symbols and text into a pattern that matches what you want
+
+*/
+
+
+// 1) Test Method
+
+let testStr = "freeCodeCamp";
+let testRegex = /Code/;
+
+testRegex.test(testStr);
