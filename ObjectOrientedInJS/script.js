5b1cd4343127f3697140c2be06fa4eff1eb34066
Shivang Suchak <shivang170695@gmail.com>
2021-08-03 19:24:46 -0700

    Understanding how to determine the value of keyword this in JS

---
+++ ObjectOrientedInJS/script.js
@@ -0,0 +1,76 @@
+// We will talk about idea of Object-oriented programming in a language which has no built in support of the same.
+
+// The idea of OOP in a traditional setting is to surrond the application around class which is a blueprint to create different instances(copies)(So, we instantiate a class). 
+
+
+// JS only has two things that could be used to replicate the behavior of OOP.: 1) Functions and 2) Objects
+
+// We will be using functions, objects, and two special keywords "this" and "new".
+
+// When you run a function in JS, there are two special keywords created for that function.
+
+function first() {
+    console.log(typeof arguments);
+    return arguments;
+
+}
+
+console.log(first(1,2,4));
+// returns array like objects : {'0': 1, '1': 2,'2': 4}
+
+// If we try to access arguments outside the function, it will throw reference error: Not defined.
+
+// NOTE: There is a difference between undefined and not defined. undefined is a value in JS.
+
+//For example.
+var name;
+// the value of name is undefined;
+console.log(name);
+
+//If you try to access variable which is NOT defined, it will give you an error.
+
+// Difference between array-like-objects and array is that 
+
+//Value of keyword this is determined when the function runs.
+
+
+//Rules to determine the value of keyword this.
+
+//1) Global Rule: 
+
+// When the keyword this is not inside an object and it is not modified using call, apply, or bind, and the new keyword is not being used, the value of keyword this is Global object.
+
+// Reason why they say everything is an object in JS can be cleared from below example:
+
+function first() {
+    this.name = 'Shivang';
+}
+
+
+console.log(global.name);
+
+// function first is not returning anything but upon calling, it puts name key on this object. Here, the value of "this" is determined by the global rule. So, //global.name returns Shivang.
+
+// This way we make a lot of global variables(in case of browser) which is not a good idea as it becomes difficult to debug.
+
+// ES5 version of writing better JS. To help us fail loudly rather than showing silent side effects.
+
+// strict-mode will ensure that we accidently do not override or accidently create global variables. 
+
+"use strict";
+
+
+function second() {
+    return this;
+}
+
+console.log(second());
+
+// If the keyword this is created inside of an object, it will refer to the closest parent object. 
+
+var obj = {
+    firstName: "Shivang",
+    whoami : this
+}
+
+console.log(obj.whoami);
---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './Components/Home';
+
+
+import './App.css';
+
+
+function App() {
+  return (
+   <div>
+   <h1>Hey, Welcome to my Website</h1>
+   </div>
+  );
+}
+
+export default App;
